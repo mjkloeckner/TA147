98774a9841a13736072a62d6369d4ccc64d34b53
Martin Kloeckner <mjkloeckner@gmail.com>
2026-04-16 23:15:08 -0300

    Add `vhdl/dev/flip_flop_d`

---
+++ vhdl/dev/flip_flop_d/flip_flop_d_tb.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity flip_flop_d_tb is
+end entity;
+
+architecture flip_flop_d_arq of flip_flop_d_tb is
+
+    constant N : integer := 4;
+
+    signal sig_D   : std_logic_vector(N-1 downto 0) := (others => '0');
+    signal sig_Q   : std_logic_vector(N-1 downto 0);
+    signal sig_clk : std_logic := '0';
+    signal sig_rst : std_logic := '0';
+    signal sig_ena : std_logic := '1';
+
+begin
+
+    flip_flop_d : entity work.flip_flop_d(flip_flop_d_arq)
+    generic map(N => N)
+    port map(
+        D   => sig_D,
+        Q   => sig_Q,
+        clk => sig_clk,
+        rst => sig_rst,
+        ena => sig_ena);
+
+    sig_D <= (others => '1') after 5 us;
+    sig_clk <= not sig_clk after 1 us;
+    sig_rst <= '1' after 10 us, '0' after 20 us;
+
+end architecture;
